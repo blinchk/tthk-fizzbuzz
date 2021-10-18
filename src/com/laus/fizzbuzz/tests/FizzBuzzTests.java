42ca0146a234c578cb182bbb5ff926678ed6155b
Nikolas Laus <bchik3@gmail.com>
2021-10-18 13:35:00 +0300

    Add "should return 0" test for FizzBuzz.

--- src/com/laus/fizzbuzz/tests/FizzBuzzTests.java
+++ src/com/laus/fizzbuzz/tests/FizzBuzzTests.java
@@ -1,4 +1,12 @@
 package com.laus.fizzbuzz.tests;
 
+import com.laus.fizzbuzz.FizzBuzz;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class FizzBuzzTests {
+    @Test
+    public void shouldReturn0() {
+        Assertions.assertEquals(FizzBuzz.of(0), ("0"));
+    }
 }
